bbd564e16a93124207eec64f057ce5a4ab5dc16c
Luigi <luigimariadamato@gmail.com>
2016-07-03 20:43:09 +0200

    Initial commit

---
+++ median-finder/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  median-finder
+//
+//  Created by Luigi Damato 2 on 7/3/16.
+//  Copyright © 2016 Luigi Damato 2. All rights reserved.
+//
+
+#include <queue>
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
